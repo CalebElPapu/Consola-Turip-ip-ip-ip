63c0a5c452745c95c02a45a6b23b3a36bffff8b1
TalvezIsac <112785896+TalvezIsac@users.noreply.github.com>
2022-11-06 18:13:32 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+console.log("Mi primera weba en la consola truco");
+
+             var nombre = "Turip ip ip ip";
+             console.log(nombre);
+
+             var apellido = "Ip ip ip turip";
+             console.log(apellido);
+
+             var nombre_completo = nombre.concat(apellido);
+             console.log(nombre_completo);
+
+             var a = 10;
+
+             var b = 5;
+
+             var sumar = a + b;
+             console.log(sumar);
+
+             var restar = a - b;
+             console.log(restar);
+             
+             var dividir = a / b;
+             console.log(dividir);
+             
+             var multipiclar = a * b;
+             console.log(multipiclar);  
+
+
+             
